496ccc9b46138354b15f17a8e4a2060f8310bf39
Stephen Edward Ng <stephenedwardng@outlook.com>
2017-07-08 17:55:28 +0100

    Begin Supplier db

--- app/src/main/java/com/example/user/contractawardmanager/Contract.java
+++ app/src/main/java/com/example/user/contractawardmanager/Contract.java
@@ -197,6 +197,4 @@ public class Contract {
         SqlRunner.closeConnection();
     }
 
-
-
 }
--- app/src/main/java/com/example/user/contractawardmanager/Supplier.java
+++ app/src/main/java/com/example/user/contractawardmanager/Supplier.java
@@ -8,6 +8,7 @@ import db.SqlRunner;
 
 public class Supplier {
 
+    private int id;
     private String name;
     private String registration;
     private int financialScore;
@@ -91,4 +92,66 @@ public class Supplier {
     public void setCourtJudgement(int courtJudgement) {
         this.courtJudgement = courtJudgement;
     }
+
+
+    // CRUD begins
+
+    public void save() {
+        String sql = String.format("INSERT INTO suppliers (name, registration, financialScore, valueScore, deliveryScore, exclude, insurance, courtJudgement) " +
+                "VALUES ('%s', %d, %d, %d, %d, %d, %d, %d)", this.name, this.registration, this.financialScore, this.valueScore, this.deliveryScore, this.exclude, this.insurance, this.courtJudgement);
+        this.id = SqlRunner.executeUpdate(sql);
+        SqlRunner.closeConnection();
+    }
+
+    public static void all() {
+        String sql = "SELECT * FROM suppliers;";
+        ResultSet rs = SqlRunner.executeQuery(sql);
+
+        try {
+            while (rs.next()) {
+                String name = rs.getString("name");
+                int registration = rs.getInt("registration");
+                int financialScore = rs.getInt("financialScore");
+                int valueScore = rs.getInt("valueScore");
+                int deliveryScore = rs.getInt("deliveryScore");
+                int exclude = rs.getInt("exclude");
+                int insurance = rs.getInt("insurance");
+                int courtJudgement = rs.getInt("courtJudgement");
+
+
+                System.out.println("Name: " + name);
+                System.out.println("Registration: " + registration);
+                System.out.println("Financial Score: " + financialScore);
+                System.out.println("Value Score: " + valueScore);
+                System.out.println("Delivery Score: " + deliveryScore);
+                System.out.println("Exclude: " + exclude);
+                System.out.println("Insurance: " + insurance);
+                System.out.println("Court Judgement: " + courtJudgement);
+            }
+        }catch (Exception ex) {
+            System.exit(0);
+        } finally {
+            SqlRunner.closeConnection();
+        }
+
+    }
+
+    public static void deleteAll() {
+        String sql = "DELETE FROM contracts;";
+        SqlRunner.executeUpdate(sql);
+        SqlRunner.closeConnection();
+    }
+
+    public void delete() {
+        String sql = String.format("DELETE FROM contracts WHERE id = %d;", this.id);
+        SqlRunner.executeUpdate(sql);
+        SqlRunner.closeConnection();
+    }
+
+    public void update() {
+        String sql = String.format("UPDATE contracts SET title = '%s', manager = '%s', supplier_id = %d, status = '%s', category = '%s', strategicImportanceRating = %d, startDate = '%s', endDate = '%s', contractedValue = '%s', actualValue = '%s', description = '%s' WHERE id = %d;", this.title, this.manager, this.supplier_id, this.status, this.category, this.strategicImportanceRating, this.startDate, this.endDate, this.contractedValue, this.actualValue, this.description);
+        SqlRunner.executeUpdate(sql);
+        SqlRunner.closeConnection();
+    }
+
 }
