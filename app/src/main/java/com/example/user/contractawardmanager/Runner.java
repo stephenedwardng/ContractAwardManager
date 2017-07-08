a667dd50af0ba9cfc79b364da5f06ea614d6a3ca
Stephen Edward Ng <stephenedwardng@outlook.com>
2017-07-08 14:27:19 +0100

    Cannot find table

---
+++ app/src/main/java/com/example/user/contractawardmanager/Runner.java
@@ -0,0 +1,20 @@
+package com.example.user.contractawardmanager;
+
+import behaviours.Cat;
+import behaviours.Status;
+
+/**
+ * Created by user on 08/07/2017.
+ */
+
+public class Runner {
+
+    public static void main(String[] args) {
+        Contract.deleteAll();
+        Contract contract = new Contract("200 MacBook Pros", "Edward Ng", 1, Status.LIVE, Cat.IT, 3, "2017-07-07", "2018-07-07", 40000, 45000, "Procurement of new laptops for new location in Aberdeen");
+
+        contract.save();
+
+        Contract.all();
+    }
+}
